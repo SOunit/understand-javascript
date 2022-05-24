2b4ab909a2feec8c30812325ce3889fcd44b1903
SOunit <workbookwork7@gmail.com>
2022-05-24 15:15:35 -0700

    add sample

---
+++ 02-execution-context/07-let.js
@@ -0,0 +1,11 @@
+{
+  let a;
+}
+console.log(a);
+
+for (let i = 0; i < 5; i++) {}
+console.log(i);
+
+// block scope
+// variable is available only inside the block
+// combination of {} and let / const
