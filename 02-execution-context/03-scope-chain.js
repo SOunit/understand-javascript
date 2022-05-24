fe65286cdcf5a0a74a6eb7834e1793aa2b01f69c
SOunit <workbookwork7@gmail.com>
2022-05-24 15:01:10 -0700

    add sample

--- 02-execution-context/03-scope-chain.js
+++ 02-execution-context/03-scope-chain.js
@@ -1,12 +1,15 @@
-function a() {
-  var myVar = 2;
+// scope chain
+// execution context has outer lexical environment ref
 
+function a() {
   function b() {
     console.log(myVar);
   }
 
+  var myVar = 2;
   b();
 }
 
 var myVar = 1;
 a();
+b();
--- 02-execution-context/04-scope-chain.js
+++ 02-execution-context/04-scope-chain.js
@@ -1,20 +1,8 @@
-function b() {
-  console.log("b-1", myVar);
-}
-
-function a() {
-  var myVar = 2;
-  b();
-}
-
-var myVar = 1;
-a();
-
 function a() {
   var myVar = 2;
 
   function b() {
-    console.log("b-2", myVar);
+    console.log(myVar);
   }
 
   b();
